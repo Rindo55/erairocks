3800cf533064065dc5198c9d0a396cc892ff0205
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-21 13:23:14 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -1,99 +1,77 @@
 import asyncio
-
+from main.modules.schedule import update_schedule
+from main.modules.usschedule import update_schedulex
+from main.modules.utils import status_text
+from main import status
+from main.modules.db import get_animesdb, get_uploads, save_animedb
 import feedparser
-
-from main import status, queue
-
+from main import queue
 from main.inline import button1
 
-from main.modules.db import get_animesdb, get_uploads, save_animedb
-
-from main.modules.schedule import update_schedule
-
-from main.modules.usschedule import update_schedulex
-
-from main.modules.utils import status_text
-
 def trim_title(title: str):
-
-    title = title.replace("[Magnet]", "")
-
-    ext = ".mkv"
-
+    title, ext = title.replace("[SubsPlease]","").strip().split("[",maxsplit=2)
+    _, ext = ext.split("]",maxsplit=2)
+    title = title.strip() + ext
+    title = title.replace("Ijiranaide, Nagatoro-san S2", "Ijiranaide, Nagatoro-san 2")
+    title = title.replace("Shinka", "Shin Shinka")
     return title
 
-async def parse():
+def trim_link(nyaa_infoHash: str):
+    magnet = "magnet:?xt=urn:btih:"
+    link = magnet + nyaa_infoHash
 
-    url = "https://www.erai-raws.info/episodes/feed/?res=1080p&type=magnet&0879fd62733b8db8535eb1be24e23f6d"
+def parse():
+    a = feedparser.parse("https://nyaa.si/?page=rss&q=SubsPlease%201080p")
+    b = a["entries"]
+    b = b[0:4]
+    data = []    
 
-    a = feedparser.parse(url)
-
-    entries = a.get("entries", [])[:10]
-
-    data = []
-
-    for entry in entries:
-
-        title = trim_title(entry.get("title", ""))
-
-        link = entry.get("link", "")
-
-        size = entry.get("erai_size", "")
-
-        if ".mkv" in title or ".mp4" in title:
-
-            data.append({"title": title, "link": link, "size": size})
-
+    for i in b:
+        item = {}
+        item['title'] = trim_title(i['title'])
+        item['size'] = i['nyaa_size']
+        item['link'] = trim_link(i['nyaa_infoHash'])
+        data.append(item)
+    data.reverse()
     return data
 
 async def auto_parser():
-
     while True:
-
         try:
-
-            await status.edit(await status_text("Parsing Rss, Fetching Magnet Links..."), reply_markup=button1)
-
+            await status.edit(await status_text("Parsing Rss, Fetching Magnet Links..."),reply_markup=button1)
         except:
-
             pass
 
-        rss = await parse()
+        rss = parse()
+        data = await get_animesdb()
+        uploaded = await get_uploads()
 
-        saved_anime = {anime["name"] for anime in await get_animesdb()}
+        saved_anime = []
+        for i in data:
+            saved_anime.append(i["name"])
 
-        uanimes = {anime["name"] for anime in await get_uploads()}
+        uanimes = []
+        for i in uploaded:
+            uanimes.append(i["name"])
+        
+        for i in rss:
+            if i["title"] not in uanimes and i["title"] not in saved_anime:
+                if ".mkv" in i["title"] or ".mp4" in i["title"]:
+                    title = i["title"]
+                    await save_animedb(title,i)
 
-        for entry in rss:
-
-            title = entry["title"]
-
-            if title not in uanimes and title not in saved_anime:
-
-                await save_animedb(title, entry)
-
-                saved_anime.add(title)
-
-        for anime in await get_animesdb():
-
-            if anime["data"] not in queue:
-
-                queue.append(anime["data"])
-
-                print("Saved", anime["name"])
+        data = await get_animesdb()
+        for i in data:
+            if i["data"] not in queue:
+                queue.append(i["data"])    
+                print("Saved ", i["name"])   
 
         try:
-
-            await status.edit(await status_text("Idle..."), reply_markup=button1)
-
+            await status.edit(await status_text("Idle..."),reply_markup=button1)
             await update_schedule()
-
             await asyncio.sleep(6)
-
             await update_schedulex()
-
         except:
-
             pass
 
         await asyncio.sleep(30)
