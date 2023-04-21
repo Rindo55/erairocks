e3515e56c6557fa60db7ff65e6c87b168cad5303
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-21 22:03:26 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -93,4 +93,4 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
             os.remove(thumbnail)
         except:
             pass
-        return x.message_id
+          return x.message_id
